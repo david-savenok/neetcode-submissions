c4fba4444e69854f303fc0a1fa68bc05dbd0989f
david-savenok <davesavenok@gmail.com>
2026-07-26 13:09:27 -0500

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        HR = [[0] * (len(nums) + 1) for _ in range(len(nums) + 1)]
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(-1, i):
+                if j == i - 1:
+                    HR[i][j] = HR[i + 1][j]
+                else:
+                    HR[i][j] = max(nums[i] + HR[i + 1][i], HR[i + 1][j])
+        for i in HR:
+            print(i)
+        return max(nums[0] + HR[1][0], HR[1][-1])
