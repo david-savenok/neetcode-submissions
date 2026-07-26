93592f561631da378c3f5b0b31e70e96bc6fd463
david-savenok <davesavenok@gmail.com>
2026-07-26 13:38:47 -0500

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        L = len(nums)
+        HR = [0] * (L + 2) 
+        noskip = 0
+        skip = 0
+        curr = 0
+
+        for i in range(L - 1, -1, -1):
+            curr = max(nums[i] + skip, noskip)
+            skip = noskip
+            noskip = curr
+
+        return curr
