c42795c8521f52786d0d3c15e344702209acbd35
david-savenok <davesavenok@gmail.com>
2026-08-17 16:51:16 -0500

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.res = []
+
+        def _generatePar(s, o_par, c_par, remaining):
+            if c_par - o_par < 0:
+                _generatePar(s + ')', o_par, c_par + 1, remaining)
+            if remaining > 0:
+                _generatePar(s + '(', o_par + 1, c_par, remaining - 1)
+            if o_par == c_par and remaining == 0:
+                self.res.append(s) 
+        
+        _generatePar('', 0, 0, n)
+        return self.res
+
+
+
+        
