c225ce248141a260471b07503152b3135a1bc6d6
david-savenok <davesavenok@gmail.com>
2026-08-27 22:17:56 -0500

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,70 @@
+class ListNode:
+    def __init__(self, p, n, k, v):
+        self.p = p
+        self.n = n
+        self.k = k
+        self.v = v
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.hashmap = {}
+        self.head = None
+        self.tail = None
+
+    def get(self, key: int) -> int:
+        print(key)
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            if node == self.head:
+                return node.v
+            elif node == self.tail:
+                self.tail = node.p
+                node.p.n = None
+            else:    
+                node.p.n = node.n
+                node.n.p = node.p
+            self.head.p = node
+            node.n = self.head
+            self.head = node
+            return node.v
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        print(key, value)
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            node.k = key
+            node.v = value
+            if node == self.head:
+                return
+            elif node == self.tail:
+                self.tail = node.p
+                node.p.n = None
+            else:    
+                node.p.n = node.n
+                node.n.p = node.p            
+            self.head.p = node
+            node.n = self.head
+            self.head = node
+            self.hashmap[key] = node
+        else:
+            self.size += 1
+            node = ListNode(None, self.head, key, value)
+            if self.head == None:
+                self.head = node
+                self.tail = node
+                self.hashmap[key] = node
+                return
+
+            self.head.p = node
+            self.head = node
+            self.hashmap[key] = node
+            if self.size > self.capacity:
+                self.size -= 1
+                self.hashmap.pop(self.tail.k)
+                self.tail.p.n = None
+                self.tail = self.tail.p
